1284213e1937801d554f323567df5409442050ac
oumaimaezarouali <oumaima.2001ezarouali@gmail.com>
2024-03-14 12:25:47 +0000

    Add Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import image1 from "@/assets/image1.jpg";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div
+        className=" flex flex-col items-center justify-center h-screen bg-cover"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6 ), rgba(0, 0, 0, 0.6)), url(${image1.src})`,
+        }}
+      >
+        <h1 className="title text-[8.5rem] italic">
+          Welcome to Oum Flavor
+        </h1>
+        <p className="text-white text-center text-2xl font-[JosefinSans] italic -mt-10">
+          The culinary expert that your fine dining restaurant has been waiting{" "}
+          <br />
+          for. Set the table for a successful online venture with ease We!{" "}
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;

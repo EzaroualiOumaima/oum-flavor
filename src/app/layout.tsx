aaf0a6df7854e5de60a16a78034d7b4d41e118c1
oumaimaezarouali <oumaima.2001ezarouali@gmail.com>
2024-04-13 00:20:07 +0000

    add publichkey

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import StoreProvider from "@/store/storeProvider";
 import { ClerkProvider } from "@clerk/nextjs";
+import env from "dotenv";
+env.config();
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +20,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body className={inter.className}>
           <Navbar />
 
           <StoreProvider> {children}</StoreProvider>
-          
         </body>
       </html>
     </ClerkProvider>
